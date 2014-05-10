be5e1bcb7bf679c45a6d66affa0e2b27b3eef498
xorrr <derxor@gmail.com>
2014-05-10 20:23:31 +0200

    play around with raphael to draw some graphics displaying the range

--- js/app.js
+++ js/app.js
@@ -20,3 +20,18 @@ App.Medias.FIXTURES = [
   { id: 1, url: 'www.google.com'},
   { id: 2, url: 'www.yahoo.com'}
 ];
+
+Handlebars.registerHelper('rangeGfx', function() {
+  // canvas at 50, 200  600*200
+  var paper = Raphael(50, 200, 600, 200);
+
+  // regular rectangle x,y,width,height,corner-rounded
+  var mainPart = paper.rect(0, 0, 500, 50, 10);
+  mainPart.attr("fill", "#0f0");
+
+  var startTime = paper.rect(50, 0, 5, 50, 2);
+  startTime.attr("fill", "#f00");
+
+  var endTime = paper.rect(250, 0, 5, 50, 2);
+  endTime.attr("fill", "#f00");
+});
