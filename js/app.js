74a2964935a9d3f0c81aa158568a92bd3b0ea691
xorrr <derxor@gmail.com>
2014-06-02 01:40:19 +0200

    implement basic rest backend functionality

--- js/app.js
+++ js/app.js
@@ -1,44 +1,45 @@
 var App = Ember.Application.create();
 
 App.Router.map(function() {
-  this.resource('medias', { path: '/' });
-  this.resource('media', { path: ':media_id' });
+  this.resource('medias', function() {
+    this.resource('media', { path: ':media_id' });
+  });
 });
 
-App.ApplicationAdapter = DS.FixtureAdapter;
+App.IndexRoute = Ember.Route.extend({
+  redirect: function() {
+    this.transitionTo('medias');
+  }
+});
 
 App.MediasRoute = Ember.Route.extend({
   model: function() {
-    return this.store.find('medias');
+    return this.store.find('media');
   }
 });
 
 App.MediaRoute = Ember.Route.extend({
   model: function(params) {
-    return this.store.find('medias', params.media_id);
+    return this.store.find('media', params.media_id);
   }
 });
 
-App.Medias = DS.Model.extend({
+App.ApplicationAdapter = DS.RESTAdapter.extend({
+  host: 'http://localhost:1337'
+});
+
+App.Media = DS.Model.extend({
+  _id: DS.attr('string'),
   url: DS.attr('string'),
   avgStartTime: DS.attr('number'),
-  avgEndTime: DS.attr('number')
+  avgEndTime: DS.attr('number'),
+  choicesByUsers: DS.attr('number')
 });
 
-App.Medias.FIXTURES = [
-  {
-    id: 1,
-    url: 'www.google.com',
-    avgStartTime: 50.5,
-    avgEndTime: 300.3
-  },
-  {
-    id: 2,
-    url: 'www.yahoo.com',
-    avgStartTime: 70.5,
-    avgEndTime: 120.7
-  }
-];
+// deal with the MongoDB ObjectId named "_id"
+App.ApplicationSerializer = DS.RESTSerializer.extend({
+  primaryKey: "_id"
+});
 
 Ember.Handlebars.helper('draw_range', function(currentMedia) {
   // canvas at 50, 200  600*200
