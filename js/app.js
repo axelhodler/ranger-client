e840eb039564d6bb8f1685eff00aedf1475508b2
xorrr <derxor@gmail.com>
2014-06-12 00:39:11 +0200

    add dragging to the time markers

--- js/app.js
+++ js/app.js
@@ -42,25 +42,31 @@ App.ApplicationSerializer = DS.RESTSerializer.extend({
 
 Ember.Handlebars.helper('draw_range', function(currentMedia) {
   $('#range_diagram').css({"heigth":"50px"});
-  var paper = Raphael("range_diagram");
-
+  var range_block = Raphael("range_diagram");
   // regular rectangle x,y,width,height,corner-rounded
-  var mainPart = paper.rect(0, 0, 500, 50, 10);
+  var mainPart = range_block.rect(0, 0, 500, 50, 10);
   mainPart.attr("fill", "#0f0");
-
-  var startTime = paper.rect(currentMedia.get('avgStartTime'), 0, 5, 50, 2);
-  startTime.attr("fill", "#f00");
-
-  var endTime = paper.rect(currentMedia.get('avgEndTime'), 0, 5, 50, 2);
-  endTime.attr("fill", "#f00");
-
-  var startText = paper.text(currentMedia.get('avgStartTime'), 50 + 10, "Start");
-  var endText = paper.text(currentMedia.get('avgEndTime'), 50 + 10, "End");
 
   var css = { "font-weight": "bold", "font-size": 16,
               "font-family": "Arial, Helvetica, sans-serif" };
+
+  // Startstuff
+  var startTime = range_block.rect(currentMedia.get('avgStartTime'), 0, 5, 50, 2);
+  startTime.attr("fill", "#f00");
+  var startText = range_block.text(currentMedia.get('avgStartTime'), 50 + 10, "Start");
   startText.attr(css);
+
+  // Endstuff
+  var endTime = range_block.rect(currentMedia.get('avgEndTime'), 0, 5, 50, 2);
+  endTime.attr("fill", "#f00");
+  var endText = range_block.text(currentMedia.get('avgEndTime'), 50 + 10, "End");
   endText.attr(css);
+
+  // startGroup draggin
+  setDragging(range_block, startTime, startText);
+
+  // endGroup dragging
+  setDragging(range_block, endTime, endText);
 });
 
 Ember.Handlebars.helper('clear_range', function() {
@@ -87,3 +93,26 @@ Ember.Handlebars.helper('format_time', function(time) {
 
   return formatted;
 });
+
+function setDragging(rangeBlock, timeMarker, timeText) {
+  var group = rangeBlock.set(timeMarker, timeText);
+  group.data("myset", group);
+
+  var onStart = function (currentX, currentY, e) {
+    var myset = this.data("myset");
+    myset.data("mytrans", this.transform());
+  }
+
+  var onMove = function (dx, dy) {
+    var myset = this.data("myset");
+    myset.transform(this.data("mytrans")+'T'+dx+','+0);
+    timeText.attr("text", Math.round(myset.getBBox().x));
+  }
+
+  var onEnd = function () {
+    var myset = this.data("myset");
+    myset.data("mytrans", this.transform());
+  }
+
+  group.drag(onMove, onStart, onEnd);
+}
